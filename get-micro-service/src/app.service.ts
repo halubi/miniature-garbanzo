20d916b05b186a45e28a643b4fa391edf2c17941
Matan <halubi15@gmail.com>
2021-12-28 20:14:04 +0200

    logs

--- get-micro-service/src/app.service.ts
+++ get-micro-service/src/app.service.ts
@@ -8,7 +8,10 @@ export class AppService {
     'http://ec2-18-194-79-132.eu-central-1.compute.amazonaws.com/rest/api/resource';
   async getItem(): Promise<any> {
     try {
-      return await this.httpService.get(this.url);
+      const item = await this.httpService.get(this.url);
+      console.log('item', item);
+
+      return item;
     } catch (error) {
       console.error('Error geting item. ', error);
       throw error;
