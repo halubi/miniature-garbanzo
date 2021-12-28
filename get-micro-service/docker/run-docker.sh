1f9477de863f35ec6803506a2488f181c427e6e2
Matan <halubi15@gmail.com>
2021-12-28 21:34:49 +0200

    env

--- get-micro-service/docker/run-docker.sh
+++ get-micro-service/docker/run-docker.sh
@@ -2,6 +2,6 @@ echo "************************************"
 echo "**********  RUN DOCKER  ************"
 echo "************************************"
 
-sudo docker run -p 80:3001 --env=http://ec2-18-194-79-132.eu-central-1.compute.amazonaws.com/rest/api/resource URL ms/get
+sudo docker run -p 80:3001 --env URL=http://ec2-18-194-79-132.eu-central-1.compute.amazonaws.com/rest/api/resource ms/get
 
 echo "DONE"
